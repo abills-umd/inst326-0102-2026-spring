b1609c7af21a289bc16db2708e60e4abd8cffbbb
Aric Bills <abills@umd.edu>
2026-04-03 10:37:38 -0400

    moved main code into a function (try_regex); added if __name__ == "__main__" statement

--- try_regex.py
+++ try_regex.py
@@ -1,11 +1,16 @@
 import re
 
 
-expr = r"\w+"
-test_str = input("Give me a string: ")
-match = re.search(expr, test_str)
-print(
-    f"The expression matched {match[0]}"
-    if match
-    else "There was no match :("
-)
+def try_regex():
+    expr = r"\w+"
+    test_str = input("Give me a string: ")
+    match = re.search(expr, test_str)
+    print(
+        f"The expression matched {match[0]}"
+        if match
+        else "There was no match :("
+    )
+
+
+if __name__ == "__main__":
+    try_regex()
